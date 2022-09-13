45ff2476f789789c4d0f5bd223113df2bfc41276
Okpe Jude Chisom <judeelrica1111@gmail.com>
2022-09-13 07:44:10 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include <main.h>
 
 /**
- * main - Prints putchar as a message.
+ * main - Prints _putchar as a message.
  *
  * Return: Always 0 (Success)
  */
@@ -17,4 +17,5 @@ int main(void)
 	}
 	_putchar('\n');
 	return (0);
+}
 

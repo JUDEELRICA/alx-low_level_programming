6004fabf72af2f3c5909ae09b2ea899650c06fbf
Okpe Jude Chisom <judeelrica1111@gmail.com>
2022-09-15 15:15:44 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isdigit - writes a function that checks for a digit 0 to 9.
+ * @c: is the int that will be used.
+ * Return: 0
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
+
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-Wrote a function that checks for uppercase character
+Wrote a function that checks for a digit (0 through 9)
